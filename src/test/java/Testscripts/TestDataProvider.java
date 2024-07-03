3b0e43718d05c2935e0ba43070d182e5b8ffee0e
SoorajNayak <70945528+sooraj-karkala@users.noreply.github.com>
2024-07-03 21:43:45 +0530

    Update TestDataProvider.java

--- src/test/java/Testscripts/TestDataProvider.java
+++ src/test/java/Testscripts/TestDataProvider.java
@@ -6,7 +6,7 @@ import org.testng.annotations.DataProvider;
 public class TestDataProvider 
 {
     @DataProvider(name = "browserAndOsProvider")
-    public Object[][] provideBrowserAndOs() 
+    public static Object[][] provideBrowserAndOs() 
     {
         return new Object[][] {
             {"Chrome", "88.0", "Windows 10"},
@@ -16,10 +16,10 @@ public class TestDataProvider
         };
         
 //        String data[][]= {                                          
-//        		{"Chrome", "88.0", "Windows 10"},
-//                {"MicrosoftEdge", "87.0", "macOS Sierra"},
-//                {"Firefox", "82.0", "Windows 7"},
-//                {"Internet Explorer", "11.0", "Windows 10"}
+//        		{"Chrome", "88", "Windows 10"},
+//                {"MicrosoftEdge", "87", "macOS Sierra"},
+//                {"Firefox", "82", "Windows 7"},
+//                {"Internet Explorer", "11", "Windows 10"}
 //			};
 //
 //        return data;
